5a889d738242335062611bd69c31e649b5576c17
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2021-07-19 20:15:10 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,10 @@
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.analogWritePin(AnalogPin.C17, 0)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.analogWritePin(AnalogPin.C17, 1023)
+})
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.analogWritePin(AnalogPin.C17, 0)
