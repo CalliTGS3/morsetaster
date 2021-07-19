e1a04be86bc52b7b7e7fb7407e61fc7e8af287c9
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2021-07-19 20:16:06 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
     pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.analogWritePin(AnalogPin.C17, 0)
+    pins.analogWritePin(AnalogPin.C17, 1023)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
     pins.digitalWritePin(DigitalPin.P1, 1)
-    pins.analogWritePin(AnalogPin.C17, 1023)
+    pins.analogWritePin(AnalogPin.C17, 0)
 })
 pins.digitalWritePin(DigitalPin.P1, 0)
-pins.analogWritePin(AnalogPin.C17, 0)
+pins.analogWritePin(AnalogPin.C17, 1023)
